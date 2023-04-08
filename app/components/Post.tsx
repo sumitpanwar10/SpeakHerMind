2e77240c98f754a7d255f19783568aab05dd621a
sumitpanwar10 <143komali>
2023-04-08 23:48:34 +0530

    Posts added

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -1,3 +1,16 @@
 'use client'
 
-expor
+export default function Post() {
+    return (
+        <div className="bg-white my8 p-8 rounded-lg">
+            <div className="flex item-center gap-2">
+                < h3 className="font-bold text-gray-700">{name}</h3>
+            </div>
+
+            <div>
+                <p className="font-bold text-gray-700">{postTitle}</p>
+            </div>
+
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@
 import axios from "axios"
 import AddPost from "./components/AddPost"
 import { useQuery } from '@tanstack/react-query'
-
+import Post from "./components/Post"
 //fetch all posts
 const allPosts = async ()=> {
   const response = await axios.get("/api/posts/getPosts")
   return response.data
 }
-export default function Home() {
+export default function Home({ name, postTitle }) {
   const { data, error, isLoading } = useQuery({
     queryFn: allPosts,
     queryKey: ["posts"],
@@ -20,7 +20,18 @@ export default function Home() {
   return ( 
     <div>
       <AddPost />
-      
+      {data?.map((post)=>(
+        <Post 
+          key={post.id}
+          postTitle={post.title}
+          name={post.user.name}
+          // avatar={post.user.image}
+          
+          // comments={post.comments}
+        />
+      )
+      )
+      }
     </div>
   )
 }
