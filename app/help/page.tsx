4e19ef1b232c7b8a7d9f64c097d55dcf327ce16a
sumitpanwar10 <143komali>
2023-04-21 19:15:06 +0530

    some helplines

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -3,8 +3,111 @@
 export default function Help(){
     
     return (
-        <div>
-            <h1 className="text-2xl font-bold text-white">Welcome back !</h1>
+        <div className="mb-20">
+            <h1 className="text-2xl font-bold text-white text-center py-4">Helplines</h1>
+            <hr className="h-px  bg-gray-200 border-0 dark:bg-gray-700"></hr>
+            <h2 className="text-lg text-yellow-400 pt-8 pb-2">
+                For women in distress:
+            </h2>
+            <div className="flex flex-col bg-white/20 rounded-md p-4">
+                <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                        <div className="overflow-hidden">
+                            
+                            <table className="min-w-full text-center text-sm font-light">
+                                <thead className="border-b font-medium text-sky-400">
+                                    <tr>
+    
+                                        <th scope="col" className="px-6 py-2 text-left">Organisation</th>
+                                        <th scope="col" className="px-6 py-2 text-left">Contact</th>
+                        
+                                    </tr>
+                                </thead>
+                                <tbody className="text-white">
+                                    <tr className="border-b">
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">National Commission for Women Helpline</td>
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">7827170170</td>    
+                                    </tr>
+                                    <tr className="border-b">
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">Central Social Welfare Board -Police Helplin</td>
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">1091/ 1291,(011)<br></br>23317004</td>    
+                                    </tr>
+                                    <tr className="border-b">
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">Shakti Shalini</td>
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">10920</td>
+                                    </tr>
+                                    <tr className="border-b">
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">Shakti Shalini - women's shelter	</td>
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">(011) 24373736/<br></br>24373737</td>
+                                    </tr>
+                                    <tr className="border-b">
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">SAARTHAK</td>
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">(011) 26853846/<br></br>26524061</td>
+                                    </tr>
+                                    <tr className="border-b">
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">All India Women's Conference	</td>
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">10921/ (011)<br></br>23389680</td>
+                                    </tr>
+                                    <tr className="border-b">
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">JAGORI</td>
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">(011) 26692700<br></br>+918800996640</td>
+                                    </tr>
+                                    <tr className="border-b">
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">Joint Women's Programme (also has branches in<br></br>Bangalore, Kolkata, Chennai)</td>
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">(011) 24619821</td>
+                                    </tr>
+                                    <tr className="border-b">
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">Sakshi - violence intervention center	</td>
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">(0124) 2562336/<br></br>5018873</td>
+                                    </tr>
+                                    <tr className="border-b">
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">Saheli - a womens organization</td>
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">(011) 24616485<br></br>(Saturdays)</td>
+                                    </tr>
+                                    <tr className="border-b">
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">Nirmal Niketan</td>
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">(011) 27859158</td>
+                                    </tr>
+                                    <tr className="border-b">
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">Nari Raksha Samiti</td>
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">(011) 23973949</td>
+                                    </tr>
+                                    <tr className="border-b">
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">RAHI Recovering and Healing from Incest. A support<br></br>centre for women survivors of child sexual abuse</td>
+                                        <td className="whitespace-nowrap px-6 py-2 text-left">(011) 26238466/ 26224042,<br></br>26227647</td>
+                                    </tr>
+                                    </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <div className="bg-white/20 rounded-md p-4">
+                <table className="table-auto">
+                    <thead>
+                        
+                        <tr className="text-sky-400">
+                            <th>Organization</th>
+                            <th>Contact</th>
+                        </tr>
+                    </thead>
+                    <tbody className="text-white">
+                        <tr>
+                            <td className="text-left">National Commission for Women Helpline</td>
+                            <td className="text-right">7827170170</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Shakti Shalini</td>
+                            <td>10920</td>  
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div> */}
         </div>  
         
     )
