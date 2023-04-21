f07afe6980979d3dc367a4deff8a23f4d50b0662
sumitpanwar10 <143komali>
2023-04-21 09:05:11 +0530

    next config updated

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   experimental: {
     appDir: true,
+    esmExternals: 'loose',
   },
   images: {
     domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
