816e2e737c4b9319922f07907bf168d64d257a5c
sumitpanwar10 <143komali>
2023-04-21 13:37:38 +0530

    font fixed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,15 @@
 import './globals.css'
 import Nav from './auth/Nav'
-import { Roboto } from "next/font/google"
 import QueryWrapper from './auth/QueryWrapper'
+import '@fontsource/roboto/latin-ext-500.css'
 
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-roboto",
-})
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`mx-4 text-lg md:mx-48 xl:mx-96 font-medium ${roboto.variable} font-sans bg-gray-900`}>
+      <body className={`mx-4 text-lg md:mx-48 xl:mx-96 font-medium font-sans bg-gray-900`} style={{ fontFamily: 'Roboto, sans-serif' }}>
+
         <QueryWrapper>
           <Nav />
           {children}
